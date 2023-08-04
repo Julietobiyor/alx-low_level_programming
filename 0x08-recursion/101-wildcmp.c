5a0b9f18bfa0194edcff75dcbf58b17ce07ef78d
julietobiyor <obiyorjuliet@gmail.com>
2023-08-03 22:52:34 -0700

    my advance task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compare two strings and return 1 if identical or 0 if otherwise
+ * @s1: pointer to string parameter
+ * @s2: pointer to string parameter
+ * Return: 0 if not identical, otherwise 1
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
